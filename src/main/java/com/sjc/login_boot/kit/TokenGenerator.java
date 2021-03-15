d689c747574299dface2127b1277f341f863d141
admin <1217594411@qq.com>
2021-03-15 14:36:44 +0800

    Revert "Initial commit"
    This reverts commit d8ddc98a

--- src/main/java/com/sjc/login_boot/kit/TokenGenerator.java
+++
@@ -1,8 +0,0 @@
-package com.sjc.login_boot.kit;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface TokenGenerator {
-    public String generate(String... strings);
-}
